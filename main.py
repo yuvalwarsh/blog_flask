49f5060f24a04123807bc27fcc6daf45fb2e976a
Yuval <yuval175@gmail.com>
2021-01-19 17:36:38 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def send_mail(name, email, phonenum, message):
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
